301522d1ce4913402bbcd4f13b7a28f2a5c0f194
Zunair Ali <zunairaliofficial1@gmail.com>
2026-09-09 00:51:16 +0500

    updating task3

--- lab01/sqe-library-management/tests/test_fine_tier.py
+++ lab01/sqe-library-management/tests/test_fine_tier.py
@@ -1,9 +1,16 @@
 import pytest
 from libraryhub.library import fine_tier
  
-@pytest.mark.parametrize('days,expected', [
-    (0, 'None'), (4, 'Low'), (10, 'Medium'), (20, 'High'), (45, 'Severe'),
-])
+@pytest.mark.parametrize(
+    'days,expected', 
+    [
+        (0, 'None'), 
+        (4, 'Low'), 
+        (10, 'Medium'), 
+        (20, 'High'), 
+        (45, 'Severe'),
+    ]
+)
 def test_fine_tier_valid_classes(days, expected):
     assert fine_tier(days) == expected
  
